201bd3a38da9a999207eae6e135697688dbea7da
dzianis.haurylavets <dzianis.haurylavets@it-wk.ru>
2019-12-25 11:10:53 +0300

    init

---
+++ src/main/java/io/dzianish/notificationbot/NotificationBotApplication.java
@@ -0,0 +1,13 @@
+package io.dzianish.notificationbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/dzianish/notificationbot/NotificationBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dzianish.notificationbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
